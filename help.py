03bfd2762421267f428529193e8f7903d366fbf0
SvetlanaPopova <sv_popova@bk.ru>
2015-07-29 20:35:22 +0300

    modified help

--- help.py
+++ help.py
@@ -1,18 +1,20 @@
-def get_list(id_folder)
+def get_list(self, element=None, w_ch_list=True):
+	wd=self....
         a = []
-        if id_folder is None:
-                el = wd
-        else:
-                el = wd.select_by_id(folder.id)
-
+        if element is None:
+                element = wd 
+	element.click()?
 	if len(el.els) > 0:
 		for el in el.els:
-			id = id
-			name = name
 			type = type
 			if el.type is a folder:
-				child = self.get_list(id)
-				a.append(Folder(id=id, name = name, child = child)
+				id = id
+				name = name
+				sub_elements = self.get_list(el, w_ch_list)
+				a.append(Folder(id=id, name = name, sub_elements = sub_elements)
 			else:
-				a.append(Checklist(id=id, name = name)
+				if w_ch_list:
+					id = id
+					name = name
+					a.append(Checklist(id=id, name = name)
         return a
